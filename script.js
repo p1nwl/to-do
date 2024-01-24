03d47acc0056aba0a93d5d518d9608dbfe8c2af8
Maxim Semchishin <max.semchishin@gmail.com>
2024-01-25 00:30:03 +0500

    local storage and return button

--- script.js
+++ script.js
@@ -1,60 +1,130 @@
-const todosContent = document.getElementById('todo-content')
-const addTodoButton = document.getElementById('addTodo')
-const todoInput = document.getElementById('todo-input')
-const todoSelect = document.getElementById('todoSelect')
+const todosContent = document.getElementById("todo-content");
+const addTodoButton = document.getElementById("addTodo");
+const todoInput = document.getElementById("todo-input");
+const todoSelect = document.getElementById("todoSelect");
+const todosArchive = document.getElementById("todo-archive");
 
-const todos = [];
+let todos = [];
 
-addTodoButton.addEventListener('click', () => {
-    if (todoInput.value === '') return;
-
-    addTodo(todoInput.value, todoSelect.value)
-    todoInput.value = ''
-    render()
-})
-
-todosContent.addEventListener('click', (event) => {
-    if (event.target !== 'BUTTON') return;
-
-})
-
-function addTodo(text, category) {
-    const todo = {
-        text,
-        category,
-        done: false,
-        id: `${Math.random()}`
-    }
-
-    todos.push(todo)
+if (localStorage.todos !== undefined) {
+  todos = JSON.parse(localStorage.getItem("todos"));
+  render();
 }
 
-function removeTodo(id) {
-    
+todoInput.addEventListener("keydown", (event) => {
+  if (todoInput.value === "") return;
+
+  if (event.key === "Enter") {
+    addTodo(todoInput.value, todoSelect.value);
+    todoInput.value = "";
+    render();
+  }
+});
+
+addTodoButton.addEventListener("click", () => {
+  if (todoInput.value === "") return;
+
+  addTodo(todoInput.value, todoSelect.value);
+  todoInput.value = "";
+  render();
+});
+
+todosContent.addEventListener("click", (event) => {
+  if (event.target.tagName !== "BUTTON") return;
+
+  const id = event.target.dataset.id;
+
+  archiveTodo(id);
+  render();
+});
+
+todosArchive.addEventListener("click", (event) => {
+  if (event.target.tagName !== "BUTTON") return;
+
+  const id = event.target.dataset.id;
+
+  unarchiveTodo(id);
+  render();
+});
+
+function addTodo(text, category) {
+  const todo = {
+    text,
+    category,
+    done: false,
+    id: `${Math.random()}`,
+    deleted: false,
+  };
+
+  todos.push(todo);
+}
+
+function archiveTodo(id) {
+  todos.forEach((todo) => {
+    if (todo.id === id) {
+      todo.done = true;
+    }
+  });
+}
+
+function unarchiveTodo(id) {
+  todos.forEach((todo) => {
+    if (todo.id === id) {
+      todo.done = false;
+    }
+  });
+}
+
+function deleteTodo(id) {
+  todos.forEach((todo) => {
+    if (todo.id === id) {
+      todo;
+    }
+  });
 }
 
 function render() {
+  todosContent.innerHTML = "";
+  todosArchive.innerHTML = "";
 
-    todosContent.innerHTML = '';
+  todos.forEach((element) => {
+    // todosContent.innerHTML += `
+    //     <div class="todos">
+    //         <p>${element.text}</p>
+    //         <button class="todo-remove">Done</button>
+    //     </div>
+    //     `
+    // if (element.done === true) return;
+    let div = document.createElement("div");
+    div.classList.add("todos");
 
-    todos.forEach(element => {
-        todosContent.innerHTML += `
-            <div class="todos">
-                <p>${element.text}</p>
-                <button class="todo-remove">Done</button>
-            </div>
-            `
-        // let div = document.createElement('div');
-        // div.classList.add('todos')
-        // todosContent.append(div);
+    if (element.done) {
+      todosArchive.append(div);
+    } else {
+      todosContent.append(div);
+    }
 
-        // let p = document.createElement('p')
-        // div.append(p);
-        // p.append(element.text)
+    let p = document.createElement("p");
+    div.append(p);
+    p.append(element.text);
 
-        // let button = document.createElement('button');
-        // button.classList.add('todo-remove')
-        // button.textContent = 'Done'
-        // div.append(button)
-    })
+    let button = document.createElement("button");
+    button.classList.add("todo-remove");
+    if (element.done) {
+      button.textContent = "Return";
+    } else {
+      button.textContent = "Done";
+    }
+
+    button.setAttribute("data-id", element.id);
+    div.append(button);
+  });
+
+  toLocalStorage();
+  console.log(todos);
+}
+
+function toLocalStorage() {
+  const string = JSON.stringify(todos);
+  localStorage.setItem("todos", string);
 }
--- tabs.js
+++ tabs.js
@@ -1,17 +1,16 @@
-const inputToggle = document.getElementById('to-do');
-const todoContent = document.getElementById('todo-content');
-const todoArchive = document.getElementById('todo-archive');
-const tabHeader = document.querySelector('.tab-header')
+const inputToggle = document.getElementById("to-do");
+const todoContent = document.getElementById("myTodos");
+const todoArchive = document.getElementById("todo-archive");
+const tabHeader = document.querySelector(".tab-header");
 
-tabHeader.addEventListener('click', (event) => {
-    if (event.target.tagName !== 'INPUT') return;
+tabHeader.addEventListener("click", (event) => {
+  if (event.target.tagName !== "INPUT") return;
 
-    if (event.target === inputToggle) {
-        todoContent.classList.add('checked')
-        todoArchive.classList.remove('checked')
-    } else {
-        todoArchive.classList.add('checked')
-        todoContent.classList.remove('checked')
-    }
-
-})
+  if (event.target === inputToggle) {
+    todoContent.classList.add("checked");
+    todoArchive.classList.remove("checked");
+  } else {
+    todoArchive.classList.add("checked");
+    todoContent.classList.remove("checked");
+  }
+});

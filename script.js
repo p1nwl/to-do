11d470a74813143de90a81b509219581943bf4b0
Maxim Semchishin <max.semchishin@gmail.com>
2024-02-04 17:16:52 +0500

    sorted archive

--- script.js
+++ script.js
@@ -9,13 +9,18 @@ let archive = new Map();
 
 if (localStorage.todos !== undefined) {
   todos = new Map(JSON.parse(localStorage.getItem("todos")));
+  todos.forEach((todo) => {
+    todo.date = new Date(todo.date);
+  });
 
   renderTodoList();
 }
 
 if (localStorage.archive !== undefined) {
   archive = new Map(JSON.parse(localStorage.getItem("archive")));
-
+  archive.forEach((todo) => {
+    todo.date = new Date(todo.date);
+  });
   renderArchive();
 }
 
@@ -68,6 +73,7 @@ function addTodo(text, category) {
     done: false,
     id: `${Math.random()}`,
     deleted: false,
+    date: new Date(),
   };
 
   todos.set(todo.id, todo);
@@ -77,7 +83,7 @@ function deleteTodo(id) {
   todos.forEach((todo) => {
     if (todo.id === id) {
       todo.deleted = true;
-      todos = todos.filter(todo => todo.deleted !== true)
+      todos = todos.filter((todo) => todo.deleted !== true);
     }
   });
 }
@@ -86,8 +92,11 @@ function archiveTodo(id) {
   todos.forEach((todo) => {
     if (todo.id === id) {
       todo.done = true;
+      const currentDate = new Date();
+      todo.date = new Date();
+
       archive.set(todo.id, todo);
-      todos.delete(todo.id)
+      todos.delete(todo.id);
     }
   });
 }
@@ -96,8 +105,10 @@ function unarchiveTodo(id) {
   archive.forEach((todo) => {
     if (todo.id === id) {
       todo.done = false;
+      todo.date = "";
+
       todos.set(todo.id, todo);
-      archive.delete(todo.id)
+      archive.delete(todo.id);
     }
   });
 }
@@ -105,63 +116,99 @@ function unarchiveTodo(id) {
 function renderTodoList() {
   todosContent.innerHTML = "";
 
-  todos.forEach((element) => {
-
-    let div = document.createElement("div");
-    let p = document.createElement("p");
-    let archiveButton = document.createElement("button");
+  todos.forEach((todo) => {
+    const div = document.createElement("div");
+    const p = document.createElement("p");
+    const archiveButton = document.createElement("button");
 
     div.classList.add("todos");
     archiveButton.classList.add("todo-archive-button");
-
-    archiveButton.setAttribute("data-id", element.id);
+    archiveButton.textContent = "Done";
+    archiveButton.setAttribute("data-id", todo.id);
 
     div.append(p);
     div.append(archiveButton);
-    p.append(element.text);
-    
-    if (element.done) {
-      archiveButton.textContent = "Return";
-      todosArchive.append(div);
-    } else {
-      archiveButton.textContent = "Done";
-      todosContent.append(div);
-    }
+    p.append(todo.text);
+    todosContent.append(div);
   });
 
   toLocalStorageTodos();
+  console.log(todos);
 }
 
 function renderArchive() {
   todosArchive.innerHTML = "";
+  let sortedArchive = new Map();
 
-  archive.forEach((element) => {
+  let monthName = {
+    0: "January",
+    1: "February",
+    2: "March",
+    3: "April",
+    4: "May",
+    5: "June",
+    6: "July",
+    7: "August",
+    8: "September",
+    9: "October",
+    10: "November",
+    11: "December",
+  };
 
-    let div = document.createElement("div");
-    let p = document.createElement("p");
-    let archiveButton = document.createElement("button");
+  archive.forEach((todo) => {
+    let dayOfMonth = todo.date.getDate();
+    let month = todo.date.getMonth();
+    let year = todo.date.getFullYear();
 
-    div.classList.add("todos");
-    archiveButton.classList.add("todo-archive-button");
-
-    archiveButton.setAttribute("data-id", element.id);
-
-    div.append(p);
-    div.append(archiveButton);
-    p.append(element.text);
-    
-    if (element.done) {
-      archiveButton.textContent = "Return";
-      todosArchive.append(div);
-    } else {
-      archiveButton.textContent = "Done";
-      todosContent.append(div);
+    if (!sortedArchive.has(year)) {
+      sortedArchive.set(year, new Map());
     }
+
+    let yearMap = sortedArchive.get(year);
+
+    if (!yearMap.has(month)) {
+      yearMap.set(month, new Map());
+    }
+
+    let monthMap = yearMap.get(month);
+
+    if (!monthMap.has(dayOfMonth)) {
+      monthMap.set(dayOfMonth, []);
+    }
+
+    monthMap.get(dayOfMonth).push(todo);
+  });
+
+  sortedArchive.forEach((yearMap, year) => {
+    yearMap.forEach((monthMap, month) => {
+      monthMap.forEach((todos, dayOfMonth) => {
+        let dateContainer = document.createElement("div");
+        dateContainer.classList.add("todo-archive-date-container");
+        dateContainer.innerHTML = `<h3 class="date-container-title">${dayOfMonth} ${monthName[month]}, ${year}</h3>`;
+
+        todos.forEach((todo) => {
+          const div = document.createElement("div");
+          const p = document.createElement("p");
+          const archiveButton = document.createElement("button");
+
+          div.classList.add("todos");
+          archiveButton.classList.add("todo-archive-button");
+          archiveButton.textContent = "Return";
+          archiveButton.setAttribute("data-id", todo.id);
+
+          div.append(p);
+          div.append(archiveButton);
+          p.append(todo.text);
+          dateContainer.append(div);
+          todosArchive.append(dateContainer);
+        });
+      });
+    });
   });
 
   toLocalStorageArchive();
+  console.log(archive);
 }
-
 
 function toLocalStorageTodos() {
   const todosString = JSON.stringify(Array.from(todos.entries()));
